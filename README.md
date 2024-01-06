9f91e8c41b44bb4636fec3f7631c1d8466c4bed7
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-06 19:46:50 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# javasctipt-ternary-operator# javasctipt-ternary-operator
---
+++ TernaryOperator.js
@@ -0,0 +1,7 @@
+var num = 10;
+var MyCondition = (num < 20) ? "Ami tomare theke boro hote parlam na" : "Yes! Ami tomar theke boro";
+console.log(MyCondition);
+
+var num = 20;
+var MyCondition = (num > 10) ? "Ami tomar theke boro" : "Ami tomar theke bor hote parlam na";
+console.log(MyCondition);
